3d6cc1c47cb38cb3e53544c733b791528435e8a1
Nicolas Maulaiz <nnicolasmaulaiz@gmail.com>
2022-08-22 09:29:34 -0300

    22-08-2022

---
+++ exercicios-sb/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,15 @@
+package br.com.cod3r.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Spring reconhece automaticamente o controlador atraves do main
+@RestController
+public class PrimeiroController {
+	
+	//Metodo get usado na requisicao do browser
+	@GetMapping(path = {"/ola", "/saudacao"})
+	public String ola() {
+		return "Olá Spring Boot! 😏"; 
+	}
+}
